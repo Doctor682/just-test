902e2c1fd94f939833c205c5af26c1d79c23f73b
Doctor682 <sashanew212@gmail.com>
2023-03-13 16:40:16 +0200

    commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>just test</title>
+</head>
+<body>
+<?php
+    echo "Hello people!";
+    echo("The Amount is : <br />");?>
+</body>
